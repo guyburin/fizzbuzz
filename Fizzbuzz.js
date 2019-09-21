16d2549cbd1ec979477ae0ec7ffc6ec81d99d256
guyburin <guyfreedom02@gmail.com>
2019-09-21 15:55:43 -0700

    Add Test 3 case

--- Fizzbuzz.js
+++ Fizzbuzz.js
@@ -1,7 +1,18 @@
 class FizzBuzz {
   static say(number) {
-    return 1;
+    if (number % 3 == 0 && number % 5 == 0) {
+      return "FizzBuzz";
+    } else if (number % 3 == 0) {
+      return "Fizz";
+    } else if (number % 5 == 0) {
+      return "Buzz";
+    } else {
+      return number;
+    }
   }
 }
 
 module.exports = FizzBuzz;
+// for (let i = 0; i < 100; i++) {
+//   console.log( say(i));
+// }
--- Fizzbuzz.test.js
+++ Fizzbuzz.test.js
@@ -4,4 +4,19 @@ describe("Test FizzBuzz", () => {
     const result = FizzBuzz.say(1);
     expect(result).toEqual(1); // assert function
   });
+
+  test("Test 3 expext Fizz", () => {
+    const result = FizzBuzz.say(3);
+    expect(result).toEqual("Fizz"); // assert function
+  });
+
+  test("Test 5 expext Buzz", () => {
+    const result = FizzBuzz.say(5);
+    expect(result).toEqual("Buzz"); // assert function
+  });
+
+  test("Test 15 expext FizzBuzz", () => {
+    const result = FizzBuzz.say(15);
+    expect(result).toEqual("FizzBuzz"); // assert function
+  });
 });
